0baa3499ca4a3b8659a7b5e0ee4b78605061bbe2
Szymon Romanko <szymonromanko8@gmail.com>
2026-06-02 16:46:46 +0200

    swap minio to seaweedfs for "cli/" tests

--- test/cli/run.sh
+++ test/cli/run.sh
@@ -13,17 +13,16 @@ docker volume create app_data
 # correctly. It is not supposed to hold any data.
 docker volume create empty_data
 
+# run s3 server and offen containers
 docker run -d -q \
-  --name minio \
+  --name s3-server \
   --network test_network \
-  --env MINIO_ROOT_USER=test \
-  --env MINIO_ROOT_PASSWORD=test \
-  --env MINIO_ACCESS_KEY=test \
-  --env MINIO_SECRET_KEY=GMusLtUmILge2by+z890kQ \
+  --env AWS_ACCESS_KEY_ID=test \
+  --env AWS_SECRET_ACCESS_KEY="test1234" \
+  --env S3_BUCKET=backup \
   -v backup_data:/data \
-  minio/minio:RELEASE.2020-08-04T23-10-51Z server /data
-
-docker exec minio mkdir -p /data/backup
+  chrislusf/seaweedfs:4.29@sha256:d47c7ee99fcb951351d7194915f4e3a5ea604a8e8871183d713907dec4fb9bf5 \
+  mini -dir /data
 
 docker run -d -q \
   --name offen \
@@ -39,18 +38,28 @@ docker run --rm -q \
   -v empty_data:/backup/empty_data \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   --env AWS_ACCESS_KEY_ID=test \
-  --env AWS_SECRET_ACCESS_KEY=GMusLtUmILge2by+z890kQ \
-  --env AWS_ENDPOINT=minio:9000 \
+  --env AWS_SECRET_ACCESS_KEY="test1234" \
+  --env AWS_ENDPOINT=s3-server:8333 \
   --env AWS_ENDPOINT_PROTO=http \
   --env AWS_S3_BUCKET_NAME=backup \
   --env BACKUP_FILENAME=test.tar.gz \
-  --env "BACKUP_FROM_SNAPSHOT=true" \
+  --env BACKUP_FROM_SNAPSHOT=true \
   --entrypoint backup \
   offen/docker-volume-backup:${TEST_VERSION:-canary}
 
+# verify db file and empty directory are present in backup
 docker run --rm -q \
-  -v backup_data:/data alpine \
-  ash -c 'tar -xvf /data/backup/test.tar.gz && test -f /backup/app_data/offen.db && test -d /backup/empty_data'
+  --env AWS_ENDPOINT_URL="http://s3-server:8333" \
+  --env AWS_ACCESS_KEY_ID=test \
+  --env AWS_SECRET_ACCESS_KEY="test1234" \
+  --env AWS_DEFAULT_REGION=us-east-1 \
+  --env AWS_S3_USE_PATH_STYLE=true \
+  --network test_network \
+  amazon/aws-cli:2.34.54 \
+  s3 cp s3://backup/test.tar.gz - | \
+docker run --rm -i \
+  alpine \
+  ash -c 'tar -xzf - -C /tmp && test -f /tmp/backup/app_data/offen.db && test -d /tmp/backup/empty_data'
 
 pass "Found relevant files in untared remote backup."
 
@@ -58,6 +67,6 @@ pass "Found relevant files in untared remote backup."
 # purpose in order to cover this setup as well.
 expect_running_containers "2"
 
-docker rm $(docker stop minio offen)
+docker rm $(docker stop s3-server offen)
 docker volume rm backup_data app_data
 docker network rm test_network
